06ab1babb37adab9d353e8e29428c0b4926d6ad5
gustavohd18 <gustavo.hernandez@acad.pucrs.br>
2021-04-28 23:21:37 -0300

    set example from documention flutter map lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
 
 void main() {
-  runApp(MyApp());
+  return runApp(MapsApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+/// This widget will be the root of application.
+class MapsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Maps Demo',
+      home: MyHomePage(),
     );
   }
 }
 
+/// This widget is the home page of the application.
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  /// Initialize the instance of the [MyHomePage] class.
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  _MyHomePageState();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late List<Model> _data;
+  late MapShapeSource _mapSource;
+
+  @override
+  void initState() {
+    _data = const <Model>[
+      Model('New South Wales', Color.fromRGBO(255, 215, 0, 1.0),
+          '       New\nSouth Wales'),
+      Model('Queensland', Color.fromRGBO(72, 209, 204, 1.0), 'Queensland'),
+      Model('Northern Territory', Color.fromRGBO(255, 78, 66, 1.0),
+          'Northern\nTerritory'),
+      Model('Victoria', Color.fromRGBO(171, 56, 224, 0.75), 'Victoria'),
+      Model('South Australia', Color.fromRGBO(126, 247, 74, 0.75),
+          'South Australia'),
+      Model('Western Australia', Color.fromRGBO(79, 60, 201, 0.7),
+          'Western Australia'),
+      Model('Tasmania', Color.fromRGBO(99, 164, 230, 1), 'Tasmania'),
+      Model('Australian Capital Territory', Colors.teal, 'ACT')
+    ];
+
+    _mapSource = MapShapeSource.asset(
+      'assets/australia.json',
+      shapeDataField: 'STATE_NAME',
+      dataCount: _data.length,
+      primaryValueMapper: (int index) => _data[index].state,
+      dataLabelMapper: (int index) => _data[index].stateCode,
+      shapeColorValueMapper: (int index) => _data[index].color,
+    );
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        height: 520,
+        child: Center(
+          child: SfMaps(
+            layers: <MapShapeLayer>[
+              MapShapeLayer(
+                source: _mapSource,
+                showDataLabels: true,
+                legend: MapLegend(MapElement.shape),
+                tooltipSettings: MapTooltipSettings(
+                    color: Colors.grey[700],
+                    strokeColor: Colors.white,
+                    strokeWidth: 2),
+                strokeColor: Colors.white,
+                strokeWidth: 0.5,
+                shapeTooltipBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      _data[index].stateCode,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
+                },
+                dataLabelSettings: MapDataLabelSettings(
+                    textStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize:
+                            Theme.of(context).textTheme.caption!.fontSize)),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+/// Collection of Australia state code data.
+class Model {
+  /// Initialize the instance of the [Model] class.
+  const Model(this.state, this.color, this.stateCode);
+
+  /// Represents the Australia state name.
+  final String state;
+
+  /// Represents the Australia state color.
+  final Color color;
+
+  /// Represents the Australia state code.
+  final String stateCode;
 }
