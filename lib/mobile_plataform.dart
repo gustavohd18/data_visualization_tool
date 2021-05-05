acc882c81c47e3502b52401f08e0c72c41f60a02
gustavohd18 <gustavo.hernandez@acad.pucrs.br>
2021-05-04 23:49:39 -0300

    add first draft to pies

--- lib/mobile_plataform.dart
+++ lib/mobile_plataform.dart
@@ -14,7 +14,6 @@ class MobilePlataform extends StatefulWidget {
 class _MobilePlataformState extends State<MobilePlataform> {
   _MobilePlataformState();
 
-
   @override
   void initState() {
     super.initState();
@@ -22,99 +21,33 @@ class _MobilePlataformState extends State<MobilePlataform> {
 
   @override
   Widget build(BuildContext context) {
-    return _buildDefaultPieChart();
-  }
-
-  /// Returns the circular  chart with pie series.
-  SfCircularChart _buildDefaultPieChart() {
-    return SfCircularChart(
-      title: ChartTitle(text:'Sales by sales person'),
-      legend: Legend(isVisible: true),
-      series: _getDefaultPieSeries(),
-    );
-  }
-
-  /// Returns the pie series.
-  List<PieSeries<ChartSampleData, String>> _getDefaultPieSeries() {
-    final List<ChartSampleData> pieData = <ChartSampleData>[
-      ChartSampleData(x: 'David', y: 13, text: 'David \n 13%'),
-      ChartSampleData(x: 'Steve', y: 24, text: 'Steve \n 24%'),
-      ChartSampleData(x: 'Jack', y: 25, text: 'Jack \n 25%'),
-      ChartSampleData(x: 'Others', y: 38, text: 'Others \n 38%'),
+    final List<ChartData> chartData = [
+      ChartData('Branco', 25, Colors.black54),
+      ChartData('Pardo', 38,  Colors.blueAccent),
+      ChartData('Negro', 34,  Colors.red),
+      ChartData('Sem identificacao', 52,  Colors.green)
     ];
-    return <PieSeries<ChartSampleData, String>>[
-      PieSeries<ChartSampleData, String>(
-          explode: true,
-          explodeIndex: 0,
-          explodeOffset: '10%',
-          dataSource: pieData,
-          xValueMapper: (ChartSampleData data, _) => data.x,
-          yValueMapper: (ChartSampleData data, _) => data.y,
-          dataLabelMapper: (ChartSampleData data, _) => data.text,
-          startAngle: 90,
-          endAngle: 90,
-          dataLabelSettings: DataLabelSettings(isVisible: true)),
-    ];
+    return Scaffold(
+        body: Center(
+            child: Container(
+                child: SfCircularChart(legend: Legend(isVisible: true),  tooltipBehavior: TooltipBehavior(enable: true),series: <CircularSeries>[
+      // Render pie chart
+      PieSeries<ChartData, String>(
+          enableTooltip: true, 
+          dataSource: chartData,
+          xValueMapper: (ChartData data, _) => data.x,
+          yValueMapper: (ChartData data, _) => data.y,
+          // Render the data label
+          dataLabelSettings:DataLabelSettings(isVisible : true)
+)
+         
+    ]))));
   }
 }
 
-class ChartSampleData {
-  /// Holds the datapoint values like x, y, etc.,
-  ChartSampleData(
-      {this.x,
-      this.y,
-      this.xValue,
-      this.yValue,
-      this.secondSeriesYValue,
-      this.thirdSeriesYValue,
-      this.pointColor,
-      this.size,
-      this.text,
-      this.open,
-      this.close,
-      this.low,
-      this.high,
-      this.volume});
-
-  /// Holds x value of the datapoint
-  final dynamic? x;
-
-  /// Holds y value of the datapoint
-  final num? y;
-
-  /// Holds x value of the datapoint
-  final dynamic? xValue;
-
-  /// Holds y value of the datapoint
-  final num? yValue;
-
-  /// Holds y value of the datapoint(for 2nd series)
-  final num? secondSeriesYValue;
-
-  /// Holds y value of the datapoint(for 3nd series)
-  final num? thirdSeriesYValue;
-
-  /// Holds point color of the datapoint
-  final Color? pointColor;
-
-  /// Holds size of the datapoint
-  final num? size;
-
-  /// Holds datalabel/text value mapper of the datapoint
-  final String? text;
-
-  /// Holds open value of the datapoint
-  final num? open;
-
-  /// Holds close value of the datapoint
-  final num? close;
-
-  /// Holds low value of the datapoint
-  final num? low;
-
-  /// Holds high value of the datapoint
-  final num? high;
-
-  /// Holds open value of the datapoint
-  final num? volume;
+class ChartData {
+  ChartData(this.x, this.y, this.color);
+  final String x;
+  final double y;
+  final Color color;
 }
